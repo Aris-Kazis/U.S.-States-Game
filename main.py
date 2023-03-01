a3221805a1ee63b158b7e5ed1f54d65f6081fe27
Aris-Kazis <aris7kazis@gmail.com>
2023-03-01 19:48:09 +0200

    files

---
+++ main.py
@@ -0,0 +1,50 @@
+import turtle
+import pandas
+
+FONT = ("Helvetica", 9, "bold")
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.setup(width=0.4, height=0.5, startx=200, starty=200)
+image = "blank_states_img.gif"
+turtle.addshape(image)
+turtle.shape(image)
+
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+
+states_cor = pandas.read_csv("50_states.csv")
+states = states_cor["state"]
+states_list = states.to_list()
+
+cor_x = 0
+cor_y = 0
+correct_answers = []
+
+while len(correct_answers) < 50:
+
+    state = turtle.Turtle()
+    state.penup()
+    state.hideturtle()
+
+    if answer_state == "Exit":
+        break
+
+    if states.isin([answer_state]).any():
+        cor_x = int(states_cor[states_cor.state == answer_state].x)
+        cor_y = int(states_cor[states_cor.state == answer_state].y)
+        state.goto(cor_x, cor_y)
+        state.write(f"{answer_state}", align="center", font=FONT)
+        if answer_state not in correct_answers:
+            correct_answers.append(answer_state)
+
+    score = len(correct_answers)
+    answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?").title()
+
+states_list = states.to_list()
+missing_states = states.to_list()
+for state in correct_answers:
+    missing_states.remove(state)
+
+missing_states = pandas.Series(missing_states)
+missing_states.to_csv("states_to_learn.csv")
